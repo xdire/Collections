bbc71bbe4baea1821c595bfec3ddb962f97e0cfc
Anton Repin <dire.one@gmail.com>
2015-10-17 00:24:52 -0400

    DoubleLinkedListFixes
    Fixes

--- Core/Collections/DoubleLinkedSet.php
+++ Core/Collections/DoubleLinkedSet.php
@@ -58,6 +58,8 @@ class DoubleLinkedSet
     /** @var \Core\Nodes\Node | null */
     public $current=null;
 
+    private $index = [];
+
     public $count = 0;
 
     /**
@@ -74,6 +76,7 @@ class DoubleLinkedSet
             $this->current = &$node;
             $this->min = &$node;
             $this->max = &$node;
+            $this->_setToIndex($key,$node);
 
             return;
 
@@ -105,7 +108,7 @@ class DoubleLinkedSet
                     $newnode->next = $node;             // new node: set current node as next
                     $newnode->prev = $prev;             // new node: set node on the left as prev
                     $node->prev = $newnode;             // set new node to the left of current node
-
+                    $this->_setToIndex($key,$newnode);
                     break;
 
                 }
@@ -123,6 +126,7 @@ class DoubleLinkedSet
 
                 // Count nodes
                 $this->count++;
+                $this->_setToIndex($key,$newnode);
                 break;
 
             }
@@ -153,7 +157,7 @@ class DoubleLinkedSet
                     $newnode->prev = $node;             // new node: set current node as previous
                     $newnode->next = $next;             // new node: set node on the right as next
                     $node->next = $newnode;             // set new node to the right of current node
-
+                    $this->_setToIndex($key,$newnode);
                     break;                              // exit
 
                 }
@@ -170,6 +174,7 @@ class DoubleLinkedSet
                 $node->prev = &$newnode;                // set current node prev pointer to new node
 
                 $this->count++;                         // add count
+                $this->_setToIndex($key,$newnode);
                 break;
 
             }
@@ -178,16 +183,254 @@ class DoubleLinkedSet
 
     }
 
-    public function getDataToArrayMinMax(){
+    /** Store Pointer to One of the Keys (if Keys are not distinct)
+     * @param mixed $key
+     * @param Node $node
+     */
+    private function _setToIndex($key,$node){
+        $this->index[$key] = &$node;
+    }
 
-        $node = $this->min;
-        $a = [];
-        while(!is_null($node)){
-            $a[$node->key] = [$node->value];
-            $node = $node->next;
+    /**
+     * @return bool
+     */
+    public function isEmpty(){
+        return $this->count < 1;
+    }
+
+    /**
+     * @return int
+     */
+    public function count(){
+        return $this->count;
+    }
+
+    /**
+     * @param mixed $key
+     * @return Node | null
+     */
+    public function getByKey($key){
+        if(isset($this->index[$key])){
+            $node = &$this->index[$key];
+            return $node;
         }
-        return $a;
+        return null;
+    }
 
+    /** @param void */
+    private function _chRoot(){
+
+        if(!is_null($this->root->prev)){
+            $node = &$this->root->prev;
+            $this->root = $node;
+        }
+        elseif(!is_null($this->root->next)){
+            $node = &$this->root->next;
+            $this->root = $node;
+        } else {
+            $this->root = null;
+        }
+
+    }
+
+    /**
+     * @param $key
+     * @return Node | |null
+     */
+    public function deleteKey($key) {
+
+        if(isset($this->index[$key])){
+
+            $node = &$this->index[$key];
+
+            if($node === $this->root){
+                $this->_chRoot();
+            }
+
+            $this->_delete($node);
+            $this->index[$key] = null;
+
+            return $node;
+
+        }
+        return null;
+
+    }
+
+    /**
+     * @return Node|null
+     */
+    public function deleteMax() {
+        if(!$this->isEmpty()) {
+
+            $node = &$this->max;
+
+            $prev = &$node->prev;
+            if(!is_null($prev))
+                $prev->next = null;
+            $this->max = &$prev;
+
+            if ($node === $this->root) {
+                $this->_chRoot();
+            }
+
+            return $node;
+        }
+        return null;
+    }
+
+    /**
+     * @return Node|null
+     */
+    public function deleteMin() {
+
+        if(!$this->isEmpty()) {
+
+            $node = &$this->min;
+            $next = &$node->next;
+            if(!is_null($node->next))
+                $next->prev = null;
+            $this->min = &$next;
+
+            if ($node === $this->root) {
+                $this->_chRoot();
+            }
+
+            return $node;
+
+        }
+        return null;
+    }
+
+    /**
+     * @param Node $node
+     */
+    private function _delete(Node &$node) {
+
+        if(!is_null($node->next) && !is_null($node->prev)){
+            $next = &$node->next;
+            $prev = &$node->prev;
+            $prev->next = &$next;
+            $next->prev = &$prev;
+        } elseif(!is_null($node->next)){
+            $this->deleteMin();
+        } elseif(!is_null($node->prev)){
+            $this->deleteMax();
+        }
+
+    }
+
+    /**
+     * @param mixed $value
+     * @return array | null
+     */
+    public function lesserThan($value) {
+
+        if(!$this->isEmpty()){
+
+            $a = [];
+            $count=0;
+            $node = $this->root;
+
+            if ($value > $node->value) {
+
+                while(!is_null($node)){
+                    if($value <= $node->value){
+                        break;  // Break on equal or greater case
+                    }
+                    $node = $node->next;
+                }
+
+            }
+
+            // Return array of data
+            if($value < $node->value){
+
+                while(!is_null($node)){
+                    if($value < $node->value){
+                        $a[$node->key] = $node;
+                        $count++;
+                    }
+                    $node = $node->prev;
+                }
+
+            }
+
+            if($count > 0) {
+                return $a;
+            }
+
+        }
+
+        return null;
+    }
+
+    /**
+     * @param mixed $value
+     * @return array | null
+     */
+    public function greaterThan($value){
+
+        if(!$this->isEmpty()){
+
+            $a = [];
+            $count=0;
+            $node = $this->root;
+
+            // Case if values are to the left
+            // Go down by the count if value to the left of root
+            // If pointer found then set node to pointer and move to next
+            if ($value < $node->value) {
+
+                while(!is_null($node)){
+                    if($value >= $node->value){
+                        break;  // Break on equal or greater case
+                    }
+                    $node = $node->prev;
+                }
+
+            }
+
+            // Return array of data
+            if($value > $node->value){
+
+                while(!is_null($node)){
+                    if($value > $node->value){
+                        $a[$node->key] = $node;
+                        $count++;
+                    }
+                    $node = $node->next;
+                }
+
+            }
+
+            if($count > 0) {
+                return $a;
+            }
+        }
+
+        return null;
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getDataToArrayMinMax() {
+
+        if(!$this->isEmpty()) {
+            $node = $this->min;
+            $a = [];
+            $count=0;
+            while (!is_null($node)) {
+                $a[$count++] = ['key'=>$node->key,'value'=>$node->value];
+                $node = $node->next;
+            }
+
+            return $a;
+        }
+
+        return null;
     }
 
 }
